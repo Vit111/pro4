abdd6edfbd098de123c1fa6347bed977210e075e
Vit111 <busuevaulia692@gmail.com>
2022-07-20 12:17:20 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,52 @@
+
+// let arr1 = [3, 45, 23, 78, 34,0,5,6];
+// let arr2 = [4, 2, 34, 4,12, 1];
+// let arr = arr1;
+// if(arr.length < arr2.length){
+//     arr = arr2;
+// }
+// let count = [];
+// for(let i = 0; i < arr.length; i++){
+//     let x = arr1[i] * arr2[i];
+//     let y = arr[i];
+//     if(x == undefined){
+//         y = arr[i];
+//     }
+//     if(isNaN(x)){
+//         count.push(y)
+//     }
+//     else{
+//         count.push(x)
+//     }
+// }
+// console.log(count)
+
+// let arr = [4,2,34,4,1,12,1,4];
+// let x = [];
+// y = [];
+// for(let i = 0; i <= arr.length; i++){
+//     for(let j = i + 1; j <= arr.length; j++){
+//         if (arr[i] == arr[j] && (arr[i] != x) ){
+//             x.push(arr[i])
+//             break;
+//         }
+//     }
+// }
+// console.log(x)
+
+// debugger;
+// let w = prompt('What is word?', '').replaceAll(' ', '');
+// let l = prompt('What is letter?', '').replaceAll(' ', '');
+// let y = 0;
+// for(let i = 0; i <= w.length; i++){
+//     if(w[i] == (l)){
+//         y += 1;
+//     }
+// }
+// if(y == 0){
+//     console.log("letter not found");
+// }
+// else if( y != 0){
+//     console.log(`The string contains the following number of "${l}" letter: ${y}`)
+// }
+
